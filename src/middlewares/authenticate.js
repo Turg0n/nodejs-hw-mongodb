c253e561a33d2bb6a82be32e06e11cb9b2312649
Turg0n <turgon1661@gmail.com>
2024-07-02 07:57:17 +0300

    Update authenticate.js

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -26,7 +26,7 @@ export const authenticate = async (req, res, next) => {
   }
 
   const isAccessTokenExpired =
-    new Date() > new Date(session.isAccessTokenValidUntil);
+    new Date() > new Date(session.accessTokenValidUntil);
 
   if (isAccessTokenExpired) {
     next(createHttpError(401, 'Access token expired'));
